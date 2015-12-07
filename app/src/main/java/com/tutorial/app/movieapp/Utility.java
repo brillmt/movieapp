db48058e48169c9bf1960986a66ed76873190332
marlon brillantes <mbrillantes@wheelsup.com>
2015-12-07 14:34:07 -0500

    master detail layout for tablet

--- app/src/main/java/com/tutorial/app/movieapp/Utility.java
+++ app/src/main/java/com/tutorial/app/movieapp/Utility.java
@@ -6,6 +6,6 @@ package com.tutorial.app.movieapp;
 public class Utility {
 
     public static final String BASE_URL = "https://api.themoviedb.org/3/movie/";
-    public static final String API_KEY = "";
+    public static final String API_KEY = "7a3d82f40add8205d2dec33bc580975d";
 
 }
